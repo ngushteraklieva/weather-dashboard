e377b3e252433dd28479dc97dd01cf67b8890b84
Nev <ngushteraklieva@gmail.com>
2024-11-11 15:24:36 +0100

    Add API request logic in index file

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,54 @@
-import currentWeather from "./current-weather.json";
-import hourlyForecast from "./hourly-forecast.json"
-import dailyForecast from "./daily-forecast.json"
+import axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(API_KEY)
 
-function getCurrentWeather() {
-    return currentWeather.current
+export async function getWeatherData(
+  endpoint,
+  place_id,
+  measurementSystem
+) {
+  const options = {
+    method: 'GET',
+    url: `https://ai-weather-by-meteosource.p.rapidapi.com/${endpoint}`,
+    params: {
+      place_id,
+      language: 'en',
+      units: measurementSystem,
+    },
+    headers: {
+      'x-rapidapi-key': API_KEY,
+      'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com',
+      
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function getHourlyForecast() {
-    return hourlyForecast.hourly.data
-}
+export async function searchPlaces(text) {
+  const options = {
+    method: 'GET',
+    url: 'https://ai-weather-by-meteosource.p.rapidapi.com/find_places',
+    params: {
+      text,
+      language: 'en',
+    },
+    headers: {
+      'x-rapidapi-key': API_KEY,
+      'x-rapidapi-host': 'ai-weather-by-meteosource.p.rapidapi.com',
+    },
+  };
 
-function getDailyForecast() {
-    return dailyForecast.daily.data
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-export { getCurrentWeather, getHourlyForecast, getDailyForecast }
