95aeed210e313b8b6533170854bfbb829ef27ed9
Nev <ngushteraklieva@gmail.com>
2024-11-11 15:38:08 +0100

    integrate ThemeContext for dynamic theme switching in App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import Header from "../src/components/Header"
-import Main from "../src/components/Main"
-import "./styles/components/App.scss"
-import "bootstrap-icons/font/bootstrap-icons.css"
+import { useContext } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import ThemeContext from './context/theme.context.js';
+import './styles/components/App.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
-  const dark = true;
+  const { dark } = useContext(ThemeContext);
+
   return (
-    <div className={`App-${dark? "dark" : "light"}`}>
-      <Header/>
-      <Main/>
+    <div className={`App-${dark ? 'dark' : 'light'}`}>
+      <Header />
+      <Main />
     </div>
   );
-} 
+}
 
 export default App;
