9b8b571c02c20e77e89ea05518e41f717caa3d79
Nev <ngushteraklieva@gmail.com>
2024-11-11 15:33:13 +0100

    create WeatherIcon component

---
+++ src/components/WeatherIcon.js
@@ -0,0 +1,12 @@
+function WeatherIcon({ iconNumber, summary }) {
+    return (
+      <img
+        src={`${process.env.PUBLIC_URL}/weather_icons/set04/big/${iconNumber}.png`}
+        alt={summary}
+        draggable={false}
+      />
+    );
+  }
+  
+  export default WeatherIcon;
+  
