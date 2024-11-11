bde0041d8d8565d233909c49dc8c7a3d03648ecc
Nev <ngushteraklieva@gmail.com>
2024-11-11 15:36:58 +0100

    Implement search functionality for dynamic city selection with results display

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,68 @@
-import React from 'react';
-import "../styles/components/Search.scss"
+import { useContext, useState } from 'react';
+import { searchPlaces } from '../api';
+import WeatherContext from '../context/weather.context';
+import '../styles/components/Search.scss';
 
 function Search() {
-    return (
-        <>
-            <div className='search-container'>
-                <div className='search-icon'>
-                    <i className='bi bi-search'></i>
+  const { setPlace } = useContext(WeatherContext);
+  const [text, setText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [openSearchResults, setOpenSearchResults] = useState(false);
+
+  async function onSearch(e) {
+    setText(e.target.value);
+    try {
+      const data = await searchPlaces(e.target.value);
+      if (data) {
+        setSearchResults(data);
+        setOpenSearchResults(data.length > 0);
+      }
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+      setSearchResults([]);
+      setOpenSearchResults(false);
+    }
+  }
+
+  const changePlace = (place) => {
+    setPlace(place);
+    setText('');
+    setOpenSearchResults(false);
+  };
+
+  return (
+    <>
+      <div className='search-container'>
+        <div className='search-icon'>
+          <i className='bi bi-search'></i>
+        </div>
+        <div className='search-input'>
+          <input
+            type='text'
+            name='search-city'
+            placeholder='Search city ...'
+            value={text}
+            onChange={onSearch}
+          />
+        </div>
+        {openSearchResults && (
+          <div className='search-results'>
+            <div className='results-container'>
+              {searchResults.map((place) => (
+                <div
+                  className='result'
+                  key={place.place_id}
+                  onClick={() => changePlace(place)}
+                >
+                  {place.name}, {place.adm_area1}, {place.country}
                 </div>
-                <div className='search-input'>
-                    <input 
-                    type='text'
-                    name='search-city'
-                    placeholder='Search city ...'
-                    />
-                </div>
+              ))}
             </div>
-        </>
-    );
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Search;
